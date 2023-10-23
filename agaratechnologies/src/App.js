a743929807dbd80dfe028342d86729fc8ea80a50
Chandru0811 <chandru08112000@gmail.com>
2023-10-23 13:31:46 +0530

    First Commit

---
+++ agaratechnologies/src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  const containerStyle = {
+    backgroundColor: "#424242",
+    color: "#fff",
+    minHeight: "100vh",
+    alignItems: "center",
+    display: "flex",
+    justifyContent: "center",
+  };
+  return (
+    <div className="App" style={containerStyle}>
+      <h1>Hii Da Amuta &#128517;</h1>
+    </div>
+  );
+}
+
+export default App;
